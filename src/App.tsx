aeb00505840b7795ecc29e605b7df330f06a9feb
Alexandre Beck <alexandrebeck2003@gmail.com>
2023-12-22 12:36:13 -0300

    testing deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
-import Home from "./pages/Home";
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-  ]);
-
   return (
     <>
-      <RouterProvider router={router} />
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+
+      <nav>
+        <Link to="/vite-react-router/">Home</Link>
+        {" | "}
+        <Link to="/vite-react-router/contact">Contact</Link>
+      </nav>
+
+      <Outlet />
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
 }
