b203f456b98c07c4553aa14e918bda1b55fb7799
Alexandre Beck <alexandrebeck2003@gmail.com>
2024-01-12 00:16:37 -0300

    started text above image

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,24 @@
 import Header from "../../components/Header";
-import { ImageContainer, WholePage } from "./styles";
+import { Image, WholePage, HeaderContainer, Teste } from "./styles";
 
 export default function Home() {
   return (
     <WholePage>
-      <Header />
-      <ImageContainer></ImageContainer>
-      <div style={{ height: "10em", backgroundColor: "pink" }} />
-      <div style={{ height: "10em", backgroundColor: "blue" }} />
-
-      <div style={{ height: "10em", backgroundColor: "pink" }} />
-      <div style={{ height: "10em", backgroundColor: "blue" }} />
-
-      <div style={{ height: "10em", backgroundColor: "pink" }} />
-      <div style={{ height: "10em", backgroundColor: "pink" }} />
-      <div style={{ height: "10em", backgroundColor: "blue" }} />
-
-      <div style={{ height: "10em", backgroundColor: "pink" }} />
-      <div style={{ height: "10em", backgroundColor: "blue" }} />
+      <HeaderContainer>
+        <Header />
+        <Image />
+        <Teste>
+          <h1 style={{ fontSize: "60px" }}>
+            Elevando Padrões Alcançando Alturas
+          </h1>
+          <p>
+            Especialistas em elevação, a BRASEQUIP oferece venda e locação de
+            equipamentos de alta performance para sua obra. Nosso compromisso
+            com a segurança e eficiência é inabalável. Descubra soluções ágeis
+            para guindastes, mini-gruas e muito mais.
+          </p>
+        </Teste>
+      </HeaderContainer>
     </WholePage>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -9,11 +9,30 @@ export const WholePage = styled.div`
   overflow-y: scroll;
 `;
 
-export const ImageContainer = styled.div`
+export const Image = styled.div`
   display: flex;
   width: 100%;
   height: 60vh;
   background-image: url("homeImage.jpg");
   background-size: cover;
   background-position-y: 20%;
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 60vh;
+  position: relative;
+  overflow-x: hidden;
+`;
+
+export const Teste = styled.div`
+  display: flex;
+  width: 36%;
+  height: 40%;
+  position: absolute;
+  right: 8%;
+  top: 48%;
+  flex-direction: column;
 `;
