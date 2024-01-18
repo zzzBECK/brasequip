00107ce93ebbdefef0e73b6ea269d64a1b6096c4
Alexandre Beck <alexandrebeck2003@gmail.com>
2024-01-18 00:06:01 -0300

    Remove unused code in Sobre page

--- src/pages/Sobre/index.tsx
+++ src/pages/Sobre/index.tsx
@@ -1,30 +1,7 @@
-import { useEffect, useState } from "react";
 import { HeaderContainer, Image, ImageOverlay, WholePage } from "../styles";
 import { Body } from "./styles";
 
 export default function Sobre() {
-  const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-
-  const isThereFlag = windowSize.width > 1100;
-
-  useEffect(() => {
-    function handleResize() {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    }
-
-    window.addEventListener("resize", handleResize);
-
-    handleResize();
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   return (
     <WholePage>
       <HeaderContainer>
