bbd51597c8b6a50e6c3d2656121c7084df0883bc
Alexandre Beck <alexandrebeck2003@gmail.com>
2024-03-13 16:56:05 -0300

    Revert "Add close button to mobile subheader menu"
    This reverts commit de9c431ecb0d034bbdbb3b517c535800197f8b29.

--- src/components/SubHeaderMobile/index.tsx
+++ src/components/SubHeaderMobile/index.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 import { FaFacebook, FaInstagram } from "react-icons/fa";
-import { IoMdClose, IoMdMenu } from "react-icons/io";
+import { IoMdMenu } from "react-icons/io";
 import { MdDarkMode } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 import theme from "../../theme/theme";
@@ -57,28 +57,7 @@ export default function SubHeaderMobile({ height }: ISubHeaderMobile) {
 
   return (
     <div>
-      {isMenuOpen ? (
-        <IoMdClose
-          size="2em"
-          style={{
-            cursor: "pointer",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        />
-      ) : (
-        <IoMdMenu
-          size="2em"
-          onClick={toggleMenu}
-          style={{
-            cursor: "pointer",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        />
-      )}
+      <IoMdMenu size="2em" onClick={toggleMenu} style={{ cursor: "pointer" }} />
       {isMenuOpen && (
         <div
           ref={menuRef}
